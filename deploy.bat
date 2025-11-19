993a9a54fe4f3a9ff65deb2364cb175a92aaef62
neo128 <e-pi@163.com>
2025-11-19 17:39:11 +0800

    build: publish static site

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-call rmdir /s /q dist
-call rmdir /s /q ..\..\..\proj\RoboCOIN
-call npm run build
-xcopy /s /e /y dist\* ..\..\..\proj\RoboCOIN\
-xcopy /s /e /y "dist\assets\DingTalk Sans-97169c5b.ttf" ..\..\..\assets\
